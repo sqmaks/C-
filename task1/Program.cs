f999525cd46add3f9228dd99c3fc11417db94488
SqMaks <sqmaks_work@bk.ru>
2024-12-07 17:13:15 +0500

    Добавили контейнер, вывели его отдельной строкой на экран, отключили приветстсвенную Фразу

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Начнём урок.");
+﻿// Console.WriteLine("Начнём урок.");
+
+
+int value = 100;
+Console.WriteLine(value);
--- task1/obj/Debug/net9.0/task1.AssemblyInfo.cs
+++ task1/obj/Debug/net9.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43585f9bd5c8619c449f3fbf205e360a74d8b3a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
