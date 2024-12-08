1edcb7ec88818b27e5577b0aa515c955a497d894
SqMaks <sqmaks_work@bk.ru>
2024-12-08 16:09:34 +0500

    add some variables

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,5 +1,14 @@
 ﻿// Console.WriteLine("Начнём урок.");
 
 
+using System.Threading.Channels;
+
 int value = 100;
 Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
